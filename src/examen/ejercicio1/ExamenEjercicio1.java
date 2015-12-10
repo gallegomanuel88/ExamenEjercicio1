7eeb6c5d9a3f7582ff4bb9568f83ebdce5316a17
galle <galle@DESKTOP-MFD5V2L>
2015-12-10 09:27:42 +0100

    Extraidos los metodos "metodo1" y "metodo2".
    Creacion de un metodo que va a dar valor a la variable "digitos". Este metodo comprueba que el numero de digitos a mostrar nunca sea menor que 1 y mayor que 5.

--- src/examen/ejercicio1/ExamenEjercicio1.java
+++ src/examen/ejercicio1/ExamenEjercicio1.java
@@ -1,35 +1,28 @@
 package examen.ejercicio1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author galle
  */
 public class ExamenEjercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     //Codificado por: sAfOrAs
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
+
     public static boolean p = false;
 
     public static void main(String arg[]) {
-        int digitos = 3;
+        int digitos = introduccionNumeroDigito();
         int nDigitos = 0;
         if (digitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
-            int aux = i;
-
-            int contador = 0;
-
-            while (aux != 0) {
-                aux = aux / 10;
-                contador++;
-            }
+            int contador = metodo2(i);
             nDigitos = contador;
 
             if (nDigitos == digitos) {
@@ -46,15 +39,7 @@ public class ExamenEjercicio1 {
                             k--;
                         }
 
-                        while (i2 <= k) {
-                            if (i % i2 == 0) {
-                                contador2++;
-                            }
-                            i2 += 2;
-                            if (contador2 == 2) {
-                                i2 = k + 1;
-                            }
-                        }
+                        contador2 = metodo1(i2, k, i, contador2);
 
                         if (contador2 == 1) {
                             p = true;
@@ -67,6 +52,37 @@ public class ExamenEjercicio1 {
                 }
             }
         }
+    }
+
+    public static int metodo2(int i) {
+        int aux = i;
+        int contador = 0;
+        while (aux != 0) {
+            aux = aux / 10;
+            contador++;
+        }
+        return contador;
+    }
+
+    public static int metodo1(int i2, int k, int i, int contador2) {
+        while (i2 <= k) {
+            if (i % i2 == 0) {
+                contador2++;
+            }
+            i2 += 2;
+            if (contador2 == 2) {
+                i2 = k + 1;
+            }
+        }
+        return contador2;
+    }
+
+    public static int introduccionNumeroDigito() {
+        int numero;
+        do {
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de digitos a mostrar"));
+        } while (numero > 5 | numero < 1);
+        return numero;
     }
 
 }
